dc628c995f2ff6c2056640be095ddf6c2ef8373e
Tom Sinclair <tsinclair@sterlingsolutions.co.uk>
2016-01-07 15:53:32 +0000

    [Init]

---
+++ src/Sterling/CascadeSoftDeletes/CascadeSoftDeletes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+use Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException;
+
+trait CascadeSoftDeletes
+{
+    public static function bootCascadeSoftDeletes()
+    {
+        // soft delete the children before deleting the parent
+        static::deleting(function ($model)
+        {
+            foreach ($model->getChildren() as $child)
+                $model->$child()
+                      ->delete();
+        });
+
+        // we need th parent to be restored before it can be attached
+        static::restored(function ($model)
+        {
+            foreach ($model->getChildren() as $child)
+                $model->$child()->onlyTrashed()
+                      ->restore();
+        });
+    }
+
+    private function getChildren()
+    {
+        if (isset($this->children))
+            return $this->children;
+
+        throw new NotAcceptableHttpException('The variable "$children" needs to be defined!');
+    }
+}
